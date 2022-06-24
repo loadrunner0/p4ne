98b2be2047099dea276430eb04bdb8e20d7842fb
LoadRunner <loadrunner0@gmail.com>
2022-06-24 18:33:31 +0300

    Lab1.6 fix

--- Lab1.6/regexp.py
+++ Lab1.6/regexp.py
@@ -3,6 +3,15 @@ import re
 from ipaddress import IPv4Interface
 import glob
 
+def ip_check(line):
+    # We only need IP addresses and not any mess with numbers and dots
+    r = re.match(r'^.+ip address (\d+\.\d+\.\d+\.\d+) (\d+\.\d+\.\d+\.\d+)', line.lower())
+    if r:
+        return IPv4Interface(f'{r.group(1)}/{r.group(2)}')
+
+    return None
+
+
 path = r'C:\Users\ma.gusmanov\Downloads\config_files'
 files = glob.glob(rf'{path}\*.txt')
 sub = "ip address"
@@ -11,9 +20,9 @@ dic = {}
 for file in files:
     with open(file) as f:
         for line in f:
-            r = re.match(r'^.+ip address ([\d.]+) ([\d.]+)', line.lower())
-            if r:
-                dic[r.group(1)] = r.group(2)
+            t = ip_check(line)
+            if t:
+                dic[t.ip] = t.netmask
 
 for key in dic:
     print(f'{key}: {dic[key]}')
